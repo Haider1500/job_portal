ee33a3bef903fe918a554a129bd798edd3871da6
Ali Haider <ali1500haider@gmail.com>
2024-09-02 01:26:28 +0800

    2nd commit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const dbConnection = require("./database/dbConnection");
 
 const app = express();
+dbConnection();
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 const app = require("./app");
-const dbConnection = require("./database/dbConnection");
 
 const port = process.env.PORT;
-dbConnection();
 app.listen(port, () => {
   console.log(`server is listening at Port ${port}`);
 });
